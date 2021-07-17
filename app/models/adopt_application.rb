adfeb5d5765c93c41d4f47d09539dd56d70a8863
Antonio King <antonio.j.king@gmail.com>
2021-07-17 16:25:05 -0600

    Add association btwn application and app_pets

--- app/models/adopt_application.rb
+++ app/models/adopt_application.rb
@@ -1,3 +1,4 @@
 class AdoptApplication < ApplicationRecord
   has_many :application_pets
+  has_many :pets, through: :application_pets
 end
--- spec/models/adopt_application_spec.rb
+++ spec/models/adopt_application_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AdoptApplication do
   describe 'relationships' do
-    it { should have_many :application_pets }
+    it {should have_many :application_pets}
+    it {should have_many(:pets).through(:application_pets)}
   end
 end
