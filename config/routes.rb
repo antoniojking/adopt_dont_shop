0160f06acaf0f0f4b0c7b10a017890fe3dc3f0be
Antonio King <antonio.j.king@gmail.com>
2021-07-18 20:30:04 -0600

    Add adopt_app index page

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,8 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
+  get '/adopt_applications', to: 'adopt_applications#index'
   get '/adopt_applications/new', to: 'adopt_applications#new'
   get '/adopt_applications/:id', to: 'adopt_applications#show'
-  post '/adopt_applications', to: 'adopt_applications#create'
+  post '/adopt_applications/:id', to: 'adopt_applications#create'
 end
